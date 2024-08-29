afd3d23809f4fa4453218b832a7525b3006816c2
ʙ ɪ ᴍ ᴀ ᴀ ᴀ <bimaprast1601@gmail.com>
2024-08-29 22:09:07 +0700

    Watchers

--- section-2-1-STARTER/app.js
+++ section-2-1-STARTER/app.js
@@ -29,6 +29,13 @@ const vm = Vue.createApp({
 
       return `${this.firstName} ${this.middleName} ${this.lastName.toUpperCase()}`
     },
+  },
+  watch: {
+    age(newVal, oldVal) {
+      setTimeout(() => {
+        this.age = 20
+      }, 3000)
+    }
   }
 }).mount('#app')
 
