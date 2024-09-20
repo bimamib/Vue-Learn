dd0cb9a58d7701bdf914844ebb5dc2e3ba9fd428
ʙ ɪ ᴍ ᴀ ᴀ ᴀ <bimaprast1601@gmail.com>
2024-09-20 23:25:48 +0700

    Emitting Events

--- components/src/App.vue
+++ components/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <h3>Hey!</h3>
   <greeting :age="age"></greeting>
-  <user :age="age"></user>
+  <user :age="age" @age-change="updateAge"></user>
 </template>
 
 <script>
@@ -17,6 +17,11 @@ export default {
   data() {
     return {
       age: 20
+    }
+  },
+  methods: {
+    updateAge(num) {
+      this.age += num
     }
   }
 }
