d28ce6967ce222c92d59c4d83a0ab3678d8f2a0d
ʙ ɪ ᴍ ᴀ ᴀ ᴀ <bimaprast1601@gmail.com>
2024-10-09 15:46:33 +0700

    Fine-tuning Transitions

--- section-7-1-STARTER/src/App.vue
+++ section-7-1-STARTER/src/App.vue
@@ -1,7 +1,8 @@
 <template>
   <button type="button" @click="flag = !flag">Toggle</button>
-  <transition name="fade">
-    <h2 v-if="flag">Hello World!</h2>
+  <transition name="fade" mode="out-in">
+    <h2 v-if="flag" key="main">Hello World!</h2>
+    <h2 v-else key="secondary">Another Hello!</h2>
   </transition>
 </template>
 
@@ -22,11 +23,11 @@ export default {
 }
 
 .fade-enter-active {
-  transition: all 0.25s linear;
+  transition: all 3s linear;
 }
 
 .fade-leave-to {
-  transition: all .25s linear;
+  transition: all 3s linear;
   opacity: 0;
 }
 </style>
