c56d1ee92947caab6257e78ba16840a22eb4bac6
ʙ ɪ ᴍ ᴀ ᴀ ᴀ <bimaprast1601@gmail.com>
2024-10-10 21:19:58 +0700

    Moving between Questions

--- quiz/src/App.vue
+++ quiz/src/App.vue
@@ -1,6 +1,11 @@
 <template>
   <div class="ctr">
-    <questions v-if="questionsAnswered < questions.length" :questions="questions" />
+    <questions 
+      v-if="questionsAnswered < questions.length" 
+      :questions="questions" 
+      :questionsAnswered="questionsAnswered"
+      @question-answered="questionAnswered" 
+    />
     <result v-else />
     <button type="button" class="reset-btn">Reset</button>
   </div>
@@ -19,6 +24,7 @@ export default {
   data() {
     return {
       questionsAnswered: 0,
+      totalCorrect: 0,
       questions: [
         {
           q: 'What is 2 + 2?',
@@ -93,10 +99,19 @@ export default {
           title: "Wow, you're a genius!",
           desc: "Studying has definitely paid off for you!"
         }
-      ]
-    }
-  }
-}
+      ],
+    };
+  },
+  methods: {
+    questionAnswered(is_correct) {
+      if (is_correct) {
+        this.totalCorrect++;
+      }
+
+      this.questionsAnswered++;
+    },
+  },
+};
 </script>
 
 <style></style>
