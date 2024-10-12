bd70debb6c69aea3b2d2563c711a0367e658aa3a
ʙ ɪ ᴍ ᴀ ᴀ ᴀ <bimaprast1601@gmail.com>
2024-10-12 21:37:06 +0700

    Using Getters

--- music/src/stores/modal.js
+++ music/src/stores/modal.js
@@ -6,4 +6,9 @@ export default defineStore("modal", {
   state: () => ({
     isOpen: false,
   }),
+  getters: {
+    hiddenClass(state) {
+      return !state.isOpen ? "hidden" : "";
+    }
+  }
 });
