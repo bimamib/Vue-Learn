aef1669ae6ad32efb0d037bd2e2689ca99b30d47
ʙ ɪ ᴍ ᴀ ᴀ ᴀ <bimaprast1601@gmail.com>
2024-10-13 20:40:18 +0700

    Applying Rules

--- music/src/includes/validation.js
+++ music/src/includes/validation.js
@@ -1,11 +1,17 @@
-import { Form as VeeForm, Field as VeeField, defineRule } from "vee-validate";
-import { required } from "@vee-validate/rules"
+import {
+  Form as VeeForm,
+  Field as VeeField,
+  defineRule,
+  ErrorMessage,
+} from 'vee-validate'
+import { required } from '@vee-validate/rules'
 
 export default {
   install(app) {
     app.component("VeeForm", VeeForm);
     app.component("VeeField", VeeField);
+    app.component("ErrorMessage", ErrorMessage);
 
-    defineRule('required', required);
+    defineRule('required', required)
   },
-};
+}
