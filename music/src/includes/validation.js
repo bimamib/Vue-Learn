133bc86a79ff5f56ac0ae2dc617998ce4ccafd2e
ʙ ɪ ᴍ ᴀ ᴀ ᴀ <bimaprast1601@gmail.com>
2024-10-14 16:01:57 +0700

    Validating Passwords

--- music/src/includes/validation.js
+++ music/src/includes/validation.js
@@ -11,7 +11,8 @@ import {
   alpha_spaces as alphaSpaces,
   email,
   min_value as minVal,
-  max_value as maxVal,
+  max_value as maxVal, 
+  confirmed,
 } from '@vee-validate/rules'
 
 export default {
@@ -27,5 +28,6 @@ export default {
     defineRule('email', email);
     defineRule('min_value', minVal);
     defineRule('max_value', maxVal);
+    defineRule('confirmed', confirmed);
   },
 }
