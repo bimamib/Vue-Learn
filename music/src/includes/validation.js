a6005c3b634a34fafe3ab7fbdee673c5b659646a
ʙ ɪ ᴍ ᴀ ᴀ ᴀ <bimaprast1601@gmail.com>
2024-10-15 20:08:55 +0700

    Validation Triggers

--- music/src/includes/validation.js
+++ music/src/includes/validation.js
@@ -62,6 +62,10 @@ export default {
 
         return message
       },
+      validateOnBlur: true,
+      validateOnChange: true,
+      validateOnInput: false,
+      validateOnModelUpdate: true,
     })
   },
 }
