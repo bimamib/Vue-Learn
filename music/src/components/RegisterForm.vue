06b3d5c0797eff8d70a4c44f70d32a22490891a3
ʙ ɪ ᴍ ᴀ ᴀ ᴀ <bimaprast1601@gmail.com>
2024-10-16 14:40:16 +0700

    Handling the Response

--- music/src/components/RegisterForm.vue
+++ music/src/components/RegisterForm.vue
@@ -140,13 +140,22 @@ export default {
       this.reg_alert_variant = 'bg-blue-500 rounded-lg'
       this.reg_alert_msg = 'Please Wait! Your Account is being created.'
 
-      const userCred = await firebase
-        .auth()
-        .createUserWithEmailAndPassword(values.email, values.password)
+      let userCred = null
+      try {
+        userCred = await firebase
+          .auth()
+          .createUserWithEmailAndPassword(values.email, values.password)
+      } catch (error) {
+        this.reg_in_submission = false
+        this.reg_alert_variant = 'bg-red-500 rounded-lg'
+        this.reg_alert_msg =
+          'An unexpected error occured. Please try again later.'
+        return
+      }
 
       this.reg_alert_variant = 'bg-green-500 rounded-lg'
       this.reg_alert_msg = 'Success! Your Account has been created.'
-      console.log(values)
+      console.log(userCred)
     },
   },
 }
