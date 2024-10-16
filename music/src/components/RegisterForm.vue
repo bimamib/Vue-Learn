fb620b7db54f41ee763967a076b52beef8a0dfe4
ʙ ɪ ᴍ ᴀ ᴀ ᴀ <bimaprast1601@gmail.com>
2024-10-16 19:21:33 +0700

    Using Actions

--- music/src/components/RegisterForm.vue
+++ music/src/components/RegisterForm.vue
@@ -108,8 +108,7 @@
 </template>
 
 <script>
-import { auth, usersCollection } from '@/includes/firebase'
-import { mapWritableState } from 'pinia'
+import { mapActions } from 'pinia'
 import useUserStore from '@/stores/user'
 
 export default {
@@ -135,22 +134,18 @@ export default {
       reg_alert_msg: 'Please Wait! Your Account is being created.',
     }
   },
-  computed: {
-    ...mapWritableState(useUserStore, ['userLoggedIn']),
-  },
   methods: {
+    ...mapActions(useUserStore, {
+      createUser: 'register',
+    }),
     async register(values) {
       this.reg_show_alert = true
       this.reg_in_submission = true
       this.reg_alert_variant = 'bg-blue-500 rounded-lg'
       this.reg_alert_msg = 'Please Wait! Your Account is being created.'
 
-      let userCred = null
       try {
-        userCred = await auth.createUserWithEmailAndPassword(
-          values.email,
-          values.password,
-        )
+        await this.createUser(values)
       } catch (error) {
         this.reg_in_submission = false
         this.reg_alert_variant = 'bg-red-500 rounded-lg'
@@ -158,27 +153,9 @@ export default {
           'An unexpected error occured. Please try again later.'
         return
       }
-
-      try {
-        await usersCollection.add({
-          name: values.name,
-          email: values.email,
-          age: values.age,
-          country: values.country,
-        })
-      } catch (error) {
-        this.reg_in_submission = false
-        this.reg_alert_variant = 'bg-red-500 rounded-lg'
-        this.reg_alert_msg =
-          'An unexpected error occured. Please try again later.'
-        return
-      }
-
-      this.userLoggedIn = true
 
       this.reg_alert_variant = 'bg-green-500 rounded-lg'
       this.reg_alert_msg = 'Success! Your Account has been created.'
-      console.log(userCred)
     },
   },
 }
