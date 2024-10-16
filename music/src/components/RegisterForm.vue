e68e1ff73047a4419521c336a8d8eaeb3cd77529
ʙ ɪ ᴍ ᴀ ᴀ ᴀ <bimaprast1601@gmail.com>
2024-10-16 18:52:46 +0700

    Logging the user in after Sign Up

--- music/src/components/RegisterForm.vue
+++ music/src/components/RegisterForm.vue
@@ -109,6 +109,8 @@
 
 <script>
 import { auth, usersCollection } from '@/includes/firebase'
+import { mapWritableState } from 'pinia'
+import useUserStore from '@/stores/user'
 
 export default {
   name: 'RegisterForm',
@@ -132,6 +134,9 @@ export default {
       reg_alert_variant: 'bg-blue-500',
       reg_alert_msg: 'Please Wait! Your Account is being created.',
     }
+  },
+  computed: {
+    ...mapWritableState(useUserStore, ['userLoggedIn']),
   },
   methods: {
     async register(values) {
@@ -168,6 +173,8 @@ export default {
           'An unexpected error occured. Please try again later.'
         return
       }
+
+      this.userLoggedIn = true
 
       this.reg_alert_variant = 'bg-green-500 rounded-lg'
       this.reg_alert_msg = 'Success! Your Account has been created.'
