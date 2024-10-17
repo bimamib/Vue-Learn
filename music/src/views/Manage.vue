4fbc975591280d270cee25d5a7bb514f603c2c06
ʙ ɪ ᴍ ᴀ ᴀ ᴀ <bimaprast1601@gmail.com>
2024-10-18 00:50:20 +0700

    Preparing the Upload Component

--- music/src/views/Manage.vue
+++ music/src/views/Manage.vue
@@ -3,70 +3,7 @@
   <section class="container mx-auto mt-6">
     <div class="md:grid md:grid-cols-3 md:gap-4">
       <div class="col-span-1">
-        <div
-          class="bg-white rounded-xl border border-gray-200 relative flex flex-col shadow"
-        >
-          <div class="px-6 pt-6 pb-5 font-bold border-b border-gray-200">
-            <span class="card-title">Upload</span>
-            <i class="fas fa-upload float-right text-green-400 text-2xl"></i>
-          </div>
-          <div class="p-6">
-            <!-- Upload Dropbox -->
-            <div
-              class="flex items-center justify-center w-full px-10 py-10 rounded-lg text-center cursor-pointer border-2 border-dashed border-gray-400 text-gray-400 transition duration-500 hover:text-white hover:bg-green-400 hover:border-green-400 hover:border-solid"
-            >
-              <div class="flex flex-col items-center justify-center pt-5 pb-6">
-                <svg
-                  class="w-8 h-8 mb-4"
-                  aria-hidden="true"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 20 16"
-                >
-                  <path
-                    stroke="currentColor"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"
-                  />
-                </svg>
-                <h5>Drop your files here</h5>
-              </div>
-            </div>
-            <hr class="my-6" />
-            <!-- Progess Bars -->
-            <div class="mb-4">
-              <!-- File Name -->
-              <div class="font-bold text-sm">Just another song.mp3</div>
-              <div class="flex h-4 overflow-hidden bg-gray-200 rounded">
-                <!-- Inner Progress Bar -->
-                <div
-                  class="transition-all progress-bar bg-blue-400"
-                  style="width: 75%"
-                ></div>
-              </div>
-            </div>
-            <div class="mb-4">
-              <div class="font-bold text-sm">Just another song.mp3</div>
-              <div class="flex h-4 overflow-hidden bg-gray-200 rounded">
-                <div
-                  class="transition-all progress-bar bg-blue-400"
-                  style="width: 35%"
-                ></div>
-              </div>
-            </div>
-            <div class="mb-4">
-              <div class="font-bold text-sm">Just another song.mp3</div>
-              <div class="flex h-4 overflow-hidden bg-gray-200 rounded">
-                <div
-                  class="transition-all progress-bar bg-blue-400"
-                  style="width: 55%"
-                ></div>
-              </div>
-            </div>
-          </div>
-        </div>
+        <app-upload />
       </div>
       <div class="col-span-2">
         <div
@@ -196,9 +133,13 @@
 
 <script>
 // import useUserStore from '@/stores/user'
+import AppUpload from '@/components/Upload.vue'
 
 export default {
   name: 'manage',
+  components: {
+    AppUpload,
+  },
   // beforeRouteEnter(to, from, next) {
   //   const store = useUserStore()
 
