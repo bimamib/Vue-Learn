80d992307152c9a5577b4a47ee4bddafe92b3bf3
ʙ ɪ ᴍ ᴀ ᴀ ᴀ <bimaprast1601@gmail.com>
2024-10-18 14:54:04 +0700

    Feat: Adding the Progress Bar

--- music/src/components/Upload.vue
+++ music/src/components/Upload.vue
@@ -40,32 +40,15 @@
       </div>
       <hr class="my-6" />
       <!-- Progess Bars -->
-      <div class="mb-4">
+      <div class="mb-4" v-for="upload in uploads" :key="upload.name">
         <!-- File Name -->
-        <div class="font-bold text-sm">Just another song.mp3</div>
+        <div class="font-bold text-sm">{{ upload.name }}</div>
         <div class="flex h-4 overflow-hidden bg-gray-200 rounded">
           <!-- Inner Progress Bar -->
           <div
             class="transition-all progress-bar bg-blue-400"
-            style="width: 75%"
-          ></div>
-        </div>
-      </div>
-      <div class="mb-4">
-        <div class="font-bold text-sm">Just another song.mp3</div>
-        <div class="flex h-4 overflow-hidden bg-gray-200 rounded">
-          <div
-            class="transition-all progress-bar bg-blue-400"
-            style="width: 35%"
-          ></div>
-        </div>
-      </div>
-      <div class="mb-4">
-        <div class="font-bold text-sm">Just another song.mp3</div>
-        <div class="flex h-4 overflow-hidden bg-gray-200 rounded">
-          <div
-            class="transition-all progress-bar bg-blue-400"
-            style="width: 55%"
+            :class="'bg-blue-400'"
+            :style="{ width: upload.current_progress + '%' }"
           ></div>
         </div>
       </div>
@@ -81,6 +64,7 @@ export default {
   data() {
     return {
       is_dragover: false,
+      uploads: [],
     }
   },
   methods: {
@@ -96,7 +80,18 @@ export default {
 
         const storageRef = storage.ref() // music-8eea1.appspot.com
         const songsRef = storageRef.child(`songs/${file.name}`) // music-8eea1.appspot.com/songs/example.mp3
-        songsRef.put(file)
+        const task = songsRef.put(file)
+
+        this.uploads.push({
+          task,
+          current_progress: 0,
+          name: file.name,
+        })
+
+        task.on('state_changed', snapshot => {
+          const proggres =
+            (snapshot.bytesTransferred / snapshot.totalBytes) * 100
+        })
       })
 
       console.log(files)
