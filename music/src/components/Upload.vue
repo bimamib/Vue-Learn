59d6d1155e042121452b499dc28f32f3e120f903
ʙ ɪ ᴍ ᴀ ᴀ ᴀ <bimaprast1601@gmail.com>
2024-10-18 21:35:44 +0700

    Storing the File Data in the Database

--- music/src/components/Upload.vue
+++ music/src/components/Upload.vue
@@ -1,15 +1,15 @@
 <template>
   <div
-    class="bg-white rounded-xl border border-gray-200 relative flex flex-col shadow"
+    class="relative flex flex-col bg-white border border-gray-200 shadow rounded-xl"
   >
     <div class="px-6 pt-6 pb-5 font-bold border-b border-gray-200">
       <span class="card-title">Upload</span>
-      <i class="fas fa-upload float-right text-green-400 text-2xl"></i>
+      <i class="float-right text-2xl text-green-400 fas fa-upload"></i>
     </div>
     <div class="p-6">
       <!-- Upload Dropbox -->
       <div
-        class="flex items-center justify-center w-full px-10 py-10 rounded-lg text-center cursor-pointer border-2 border-dashed border-gray-400 text-gray-400 transition duration-500 hover:text-white hover:bg-green-400 hover:border-green-400 hover:border-solid"
+        class="flex items-center justify-center w-full px-10 py-10 text-center text-gray-400 transition duration-500 border-2 border-gray-400 border-dashed rounded-lg cursor-pointer hover:text-white hover:bg-green-400 hover:border-green-400 hover:border-solid"
         :class="{ 'bg-green-400 border-green-400 border-solid': is_dragover }"
         @drag.prevent.stop=""
         @dragstart.prevent.stop=""
@@ -42,7 +42,7 @@
       <!-- Progess Bars -->
       <div class="mb-4" v-for="upload in uploads" :key="upload.name">
         <!-- File Name -->
-        <div class="font-bold text-sm" :class="upload.text_class">
+        <div class="text-sm font-bold" :class="upload.text_class">
           <i :class="upload.icon"></i> {{ upload.name }}
         </div>
         <div class="flex h-4 overflow-hidden bg-gray-200 rounded">
@@ -59,7 +59,7 @@
 </template>
 
 <script>
-import { storage } from '@/includes/firebase'
+import { storage, auth, songsCollection } from '@/includes/firebase'
 
 export default {
   name: 'Upload',
@@ -107,7 +107,19 @@ export default {
             this.uploads[uploadIndex].text_class = 'text-red-400'
             console.log(error)
           },
-          () => {
+          async () => {
+            const song = {
+              uid: auth.currentUser.uid,
+              display_name: auth.currentUser.displayName,
+              original_name: task.snapshot.ref.name,
+              modified_name: task.snapshot.ref.name,
+              genre: '',
+              comment_count: 0,
+            }
+
+            song.url = await task.snapshot.ref.getDownloadURL()
+            await songsCollection.add(song)
+
             this.uploads[uploadIndex].variant = 'bg-green-400'
             this.uploads[uploadIndex].icon = 'fas fa-check'
             this.uploads[uploadIndex].text_class = 'text-green-400'
