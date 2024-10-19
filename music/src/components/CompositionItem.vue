a32d0e09a66c8cb32b3415d09e138f73c9bc326c
ʙ ɪ ᴍ ᴀ ᴀ ᴀ <bimaprast1601@gmail.com>
2024-10-19 17:44:21 +0700

    Feat: Editing a Song

--- music/src/components/CompositionItem.vue
+++ music/src/components/CompositionItem.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="p-3 mb-4 border border-gray-200 rounded-lg">
     <div v-show="!showForm">
-      <h4 class="inline-block text-2xl font-bold">{{ song.modified_name }}</h4>
+      <h4 class="inline-block text-base font-bold">{{ song.modified_name }}</h4>
       <button
         class="float-right px-2 py-1 ml-1 text-sm text-white bg-red-600 rounded"
       >
@@ -15,6 +15,13 @@
       </button>
     </div>
     <div v-show="showForm">
+      <div
+        class="p-4 mb-4 font-bold text-center text-white"
+        v-if="show_alert"
+        :class="alert_variant"
+      >
+        {{ alert_message }}
+      </div>
       <vee-form
         :validation-schema="schema"
         :initial-values="song"
@@ -43,12 +50,15 @@
         <button
           type="submit"
           class="py-1.5 px-3 rounded text-white bg-green-600"
+          :disabled="in_submission"
         >
           Submit
         </button>
         <button
           type="button"
           class="py-1.5 px-3 rounded text-white bg-gray-600"
+          :disabled="in_submission"
+          @click.prevent="showForm = false"
         >
           Go Back
         </button>
@@ -57,11 +67,21 @@
   </div>
 </template>
 <script>
+import { songsCollection } from '@/includes/firebase'
+
 export default {
   name: 'CompositionItem',
   props: {
     song: {
       type: Object,
+      required: true,
+    },
+    updateSong: {
+      type: Function,
+      required: true,
+    },
+    index: {
+      type: Number,
       required: true,
     },
   },
@@ -72,11 +92,37 @@ export default {
         modified_name: 'required',
         genre: 'alpha_spaces',
       },
+      in_submission: false,
+      show_alert: false,
+      alert_variant:
+        'max-w-xs bg-blue-100 border border-blue-200 text-sm text-blue-800 rounded-lg ',
+      alert_message: 'Please wait! Updating song info.',
     }
   },
   methods: {
-    edit() {
-      console.log('Song edited')
+    async edit(values) {
+      this.in_submission = true
+      this.show_alert = true
+      this.alert_variant =
+        'max-w-xs bg-blue-100 border border-blue-200 text-sm text-blue-800 rounded-lg'
+      this.alert_message = 'Please wait! Updating song info.'
+
+      try {
+        await songsCollection.doc(this.song.docID).update(values)
+      } catch (error) {
+        this.in_submission = false
+        this.alert_variant =
+          'max-w-xs bg-red-100 border border-red-200 text-sm text-red-800 rounded-lg'
+        this.alert_message = 'Something went wrong! Try again later'
+        return
+      }
+
+      this.updateSong(this.index, values)
+
+      this.in_submission = false
+      this.alert_variant =
+        'max-w-xs bg-teal-100 border border-teal-200 text-sm text-teal-800 rounded-lg'
+      this.alert_message = 'Success!'
     },
   },
 }
