1619489226251dacbd1255c5d0bd9d165407401c
ʙ ɪ ᴍ ᴀ ᴀ ᴀ <bimaprast1601@gmail.com>
2024-10-20 00:51:13 +0700

    Feat: Creating the Song Template

--- music/src/components/SongItem.vue
+++ music/src/components/SongItem.vue
@@ -3,9 +3,12 @@
     class="flex items-center justify-between p-3 pl-6 transition duration-300 cursor-pointer hover:bg-gray-50"
   >
     <div>
-      <a href="#" class="block font-bold text-gray-600">{{
-        song.modified_name
-      }}</a>
+      <router-link
+        :to="{ name: 'song', params: { id: song.docID } }"
+        class="block font-bold text-gray-600"
+      >
+        {{ song.modified_name }}
+      </router-link>
       <span class="text-sm text-gray-500">{{ song.display_name }}</span>
     </div>
 
