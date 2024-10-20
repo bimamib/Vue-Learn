1b4a85b706018d7f0e7aab9004bb4b3ce4190eb5
ʙ ɪ ᴍ ᴀ ᴀ ᴀ <bimaprast1601@gmail.com>
2024-10-20 21:59:07 +0700

    Feat: Updating the Comment Count

--- music/src/views/Song.vue
+++ music/src/views/Song.vue
@@ -27,7 +27,7 @@
     >
       <div class="px-6 pt-6 pb-5 font-bold border-b border-gray-200">
         <!-- Comment Count -->
-        <span class="card-title">Comments (15)</span>
+        <span class="card-title">Comments ({{ song.comment_count }})</span>
         <i class="float-right text-2xl text-green-400 fa fa-comments"></i>
       </div>
       <div class="p-6">
@@ -155,6 +155,11 @@ export default {
 
       await commentsCollection.add(comment)
 
+      this.song.comment_count += 1
+      await songsCollection.doc(this.$route.params.id).update({
+        comment_count: this.song.comment_count,
+      })
+
       this.getComments()
 
       this.comment_in_submission = false
@@ -164,7 +169,6 @@ export default {
 
       resetForm()
     },
-
     async getComments() {
       const snapshots = await commentsCollection
         .where('sid', '==', this.$route.params.id)
