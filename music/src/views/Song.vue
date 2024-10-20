57893d46d768e51842d2ff4a69f85e68f73b5c27
ʙ ɪ ᴍ ᴀ ᴀ ᴀ <bimaprast1601@gmail.com>
2024-10-20 12:21:56 +0700

    Feat: Finalizing the Comment Form

--- music/src/views/Song.vue
+++ music/src/views/Song.vue
@@ -38,7 +38,11 @@
         >
           {{ comment_alert_message }}
         </div>
-        <vee-form :validation-schema="schema" @submit="addComment">
+        <vee-form
+          :validation-schema="schema"
+          @submit="addComment"
+          v-if="userLoggedIn"
+        >
           <vee-field
             as="textarea"
             name="comment"
@@ -142,7 +146,9 @@
 </template>
 
 <script>
-import { songsCollection } from '@/includes/firebase'
+import { songsCollection, auth, commentsCollection } from '@/includes/firebase'
+import { mapState } from 'pinia'
+import useUserStore from '@/stores/user'
 
 export default {
   name: 'Song',
@@ -159,6 +165,9 @@ export default {
       comment_alert_message: 'Please wait! Your comment is being submitted',
     }
   },
+  computed: {
+    ...mapState(useUserStore, ['userLoggedIn']),
+  },
   async created() {
     const docSnapshot = await songsCollection.doc(this.$route.params.id).get()
 
@@ -170,13 +179,30 @@ export default {
     this.song = docSnapshot.data()
   },
   methods: {
-    async addComment(values) {
+    async addComment(values, { resetForm }) {
       this.comment_in_submission = true
       this.comment_show_alert = true
       this.comment_alert_variant =
         'bg-blue-100 border border-blue-200 text-sm text-blue-800 rounded-lg'
       this.comment_alert_message =
         'Please wait! Your comment is being submitted'
+
+      const comment = {
+        content: values.comment,
+        datePosted: new Date().toString(),
+        sid: this.$route.params.id,
+        name: auth.currentUser.displayName,
+        uid: auth.currentUser.uid,
+      }
+
+      await commentsCollection.add(comment)
+
+      this.comment_in_submission = false
+      this.comment_alert_variant =
+        'bg-teal-100 border border-teal-200 text-sm text-teal-800 rounded-lg'
+      this.comment_alert_message = 'Comment added!'
+
+      resetForm()
     },
   },
 }
