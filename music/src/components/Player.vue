e61ca7ff2d36beba5c0c58dd19aa0045a090b315
ʙ ɪ ᴍ ᴀ ᴀ ᴀ <bimaprast1601@gmail.com>
2024-10-21 15:12:33 +0700

    Feat: Toogling Audio

---
+++ music/src/components/Player.vue
@@ -0,0 +1,53 @@
+<template>
+  <!-- Player -->
+  <div class="fixed bottom-0 left-0 w-full px-4 py-2 bg-white">
+    <!-- Track Info -->
+    <div class="text-center">
+      <span class="font-bold song-title">Song Title</span> by
+      <span class="song-artist">Artist</span>
+    </div>
+    <div class="flex items-center gap-4 flex-nowrap">
+      <!-- Play/Pause Button -->
+      <button type="button" @click.prevent="toggleAudio">
+        <i
+          class="text-xl text-gray-500 fa"
+          :class="{ 'fa-play': !playing, 'fa-pause': playing }"
+        ></i>
+      </button>
+      <!-- Current Position -->
+      <div class="player-currenttime">00:00</div>
+      <!-- Scrub Container  -->
+      <div class="relative w-full h-2 bg-gray-200 rounded cursor-pointer">
+        <!-- Player Ball -->
+        <span
+          class="absolute -top-2.5 -ml-2.5 text-gray-800 text-lg"
+          style="left: 50%"
+        >
+          <i class="fas fa-circle"></i>
+        </span>
+        <!-- Player Progress Bar-->
+        <span
+          class="block h-2 rounded bg-gradient-to-r from-green-500 to-green-400"
+          style="width: 50%"
+        ></span>
+      </div>
+      <!-- Duration -->
+      <div class="player-duration">03:06</div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapActions, mapState } from 'pinia'
+import usePlayerStore from '@/stores/player'
+
+export default {
+  name: 'Player',
+  methods: {
+    ...mapActions(usePlayerStore, ['toggleAudio']),
+  },
+  computed: {
+    ...mapState(usePlayerStore, ['playing']),
+  },
+}
+</script>
