8e41402c0db5dc9cf8508af1c86baa392a49c3df
ʙ ɪ ᴍ ᴀ ᴀ ᴀ <bimaprast1601@gmail.com>
2024-10-21 17:06:10 +0700

    Feat: Player Progress Bar

--- music/src/components/Player.vue
+++ music/src/components/Player.vue
@@ -2,9 +2,10 @@
   <!-- Player -->
   <div class="fixed bottom-0 left-0 w-full px-4 py-2 bg-white">
     <!-- Track Info -->
-    <div class="text-center">
-      <span class="font-bold song-title">Song Title</span> by
-      <span class="song-artist">Artist</span>
+    <div class="text-center" v-if="current_song.modified_name">
+      <span class="font-bold song-title">{{ current_song.modified_name }}</span>
+      by
+      <span class="song-artist">{{ current_song.display_name }}</span>
     </div>
     <div class="flex items-center gap-4 flex-nowrap">
       <!-- Play/Pause Button -->
@@ -21,14 +22,14 @@
         <!-- Player Ball -->
         <span
           class="absolute -top-2.5 -ml-2.5 text-gray-800 text-lg"
-          style="left: 50%"
+          :style="{ left: playerProgress }"
         >
           <i class="fas fa-circle"></i>
         </span>
         <!-- Player Progress Bar-->
         <span
           class="block h-2 rounded bg-gradient-to-r from-green-500 to-green-400"
-          style="width: 50%"
+          :style="{ width: playerProgress }"
         ></span>
       </div>
       <!-- Duration -->
@@ -47,7 +48,13 @@ export default {
     ...mapActions(usePlayerStore, ['toggleAudio']),
   },
   computed: {
-    ...mapState(usePlayerStore, ['playing', 'duration', 'seek']),
+    ...mapState(usePlayerStore, [
+      'playing',
+      'duration',
+      'seek',
+      'playerProgress',
+      'current_song',
+    ]),
   },
 }
 </script>
