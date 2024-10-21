d3e591b8963a4bf421c8f93a347f77c7c367d24d
ʙ ɪ ᴍ ᴀ ᴀ ᴀ <bimaprast1601@gmail.com>
2024-10-21 18:51:09 +0700

    Feat: Changing the Audio Position

--- music/src/components/Player.vue
+++ music/src/components/Player.vue
@@ -18,7 +18,10 @@
       <!-- Current Position -->
       <div class="player-currenttime">{{ seek }}</div>
       <!-- Scrub Container  -->
-      <div class="relative w-full h-2 bg-gray-200 rounded cursor-pointer">
+      <div
+        @click.prevent="updateSeek"
+        class="relative w-full h-2 bg-gray-200 rounded cursor-pointer"
+      >
         <!-- Player Ball -->
         <span
           class="absolute -top-2.5 -ml-2.5 text-gray-800 text-lg"
@@ -45,7 +48,7 @@ import usePlayerStore from '@/stores/player'
 export default {
   name: 'Player',
   methods: {
-    ...mapActions(usePlayerStore, ['toggleAudio']),
+    ...mapActions(usePlayerStore, ['toggleAudio', 'updateSeek']),
   },
   computed: {
     ...mapState(usePlayerStore, [
