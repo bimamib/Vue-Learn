4ecafffd59ca866e47545c6942979a53070ebe7b
ʙ ɪ ᴍ ᴀ ᴀ ᴀ <bimaprast1601@gmail.com>
2024-10-25 15:49:39 +0700

    Feat: Generating the Manifest File

--- music/vite.config.js
+++ music/vite.config.js
@@ -1,22 +1,23 @@
-import {
-  fileURLToPath,
-  URL
-} from 'node:url'
+import { fileURLToPath, URL } from 'node:url'
 
-import {
-  defineConfig
-} from 'vite'
+import { defineConfig } from 'vite'
+import { VitePWA } from 'vite-plugin-pwa'
 import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      devOptions: {
+        enabled: true,
+      },
+    }),
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src',
-        import.meta.url))
-    }
-  }
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
 })
