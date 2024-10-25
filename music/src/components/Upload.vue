5bca8858133febceb9ce59101ccb3971b2aef62c
ʙ ɪ ᴍ ᴀ ᴀ ᴀ <bimaprast1601@gmail.com>
2024-10-25 20:55:41 +0700

    Feat: Handling Offline Uploads

--- music/src/components/Upload.vue
+++ music/src/components/Upload.vue
@@ -89,6 +89,18 @@ export default {
           return
         }
 
+        if (!navigator.onLine) {
+          this.uploads.push({
+            task: {},
+            current_progress: 100,
+            name: file.name,
+            variant: 'bg-red-400',
+            icon: 'fas fa-times',
+            text_class: 'text-red-400',
+          })
+          return
+        }
+
         const storageRef = storage.ref() // music-8eea1.appspot.com
         const songsRef = storageRef.child(`songs/${file.name}`) // music-8eea1.appspot.com/songs/example.mp3
         const task = songsRef.put(file)
