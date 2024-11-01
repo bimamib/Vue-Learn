fb6fb18d357f07314a3f2bde59f626527638bb54
ʙ ɪ ᴍ ᴀ ᴀ ᴀ <bimaprast1601@gmail.com>
2024-11-01 20:27:00 +0700

    Feat: Reactive References

--- section-21-3-STARTER/src/App.vue
+++ section-21-3-STARTER/src/App.vue
@@ -1,9 +1,26 @@
 <template>
-  <div></div>
+  <div>
+    <p>{{ num }}</p>
+    <button type="button" @click.prevent="increment">Click Me</button>
+  </div>
 </template>
 
 <script>
+import { ref } from "vue";
+
 export default {
   name: "App",
+  setup() {
+    let num = ref(0);
+
+    function increment() {
+      num.value++;
+    }
+
+    return {
+      num,
+      increment,
+    };
+  },
 };
 </script>
