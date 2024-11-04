3e1f2301660de3c9507a87490fd7d5f31b1a999a
ʙ ɪ ᴍ ᴀ ᴀ ᴀ <bimaprast1601@gmail.com>
2024-11-05 00:27:12 +0700

    Feat: Watchers and Computed Properties

--- section-21-3-STARTER/src/App.vue
+++ section-21-3-STARTER/src/App.vue
@@ -1,14 +1,20 @@
 <template>
   <div>
     <p>{{ num }}</p>
+    <p>{{ double }}</p>
     <button type="button" @click.prevent="increment">Click Me</button>
 
     <p>{{ name }}</p>
+
+    <p>
+      <input type="text" v-model="phrase" />
+    </p>
+    <p>{{ reversedPhrase }}</p>
   </div>
 </template>
 
 <script>
-import { ref, reactive, toRefs } from "vue";
+import { ref, reactive, toRefs, watchEffect, watch, computed } from "vue";
 
 export default {
   name: "App",
@@ -19,19 +25,37 @@ export default {
       num.value++;
     }
 
-    setTimeout(() => {
-      user.name = "Potter";
-    }, 3000);
+    const double = computed(() => {
+      return num.value * 2;
+    });
 
     const user = reactive({
       name: "Harry",
       age: 20,
     });
 
+    setTimeout(() => {
+      user.name = "Potter";
+    }, 3000);
+
+    const phrase = ref("");
+    const reversedPhrase = ref("");
+
+    // watch([phrase], ([newVal, oldVal]) => {
+    //   reversedPhrase.value = phrase.value.split("").reverse().join("");
+    // });
+
+    watchEffect(() => {
+      reversedPhrase.value = phrase.value.split("").reverse().join("");
+    });
+
     return {
       num,
       increment,
       ...toRefs(user),
+      phrase,
+      reversedPhrase,
+      double,
     };
   },
 };
