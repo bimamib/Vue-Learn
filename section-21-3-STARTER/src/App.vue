924225748ae384c318ef874d25c828b5103af741
ʙ ɪ ᴍ ᴀ ᴀ ᴀ <bimaprast1601@gmail.com>
2024-11-06 15:31:02 +0700

    Feat: The setup Attrubute

--- section-21-3-STARTER/src/App.vue
+++ section-21-3-STARTER/src/App.vue
@@ -17,54 +17,36 @@
   </div>
 </template>
 
-<script>
+<script setup>
 import { ref, reactive, toRefs, onBeforeMount, onMounted } from "vue";
 import AppAlert from "@/components/Alert.vue";
 import { useNumber } from "@/hooks/number";
 import { usePhrase } from "@/hooks/phrase";
 
-export default {
-  name: "App",
-  components: {
-    AppAlert,
-  },
-  setup() {
-    const btn = ref(null);
+const btn = ref(null);
 
-    onBeforeMount(() => {
-      console.log("onBeforeMount()");
-    });
-    onMounted(() => {
-      console.log("onMounted()");
+onBeforeMount(() => {
+  console.log("onBeforeMount()");
+});
+onMounted(() => {
+  console.log("onMounted()");
 
-      btn.value.addEventListener("click", () => {
-        console.log("button clicked");
-      });
-    });
+  btn.value.addEventListener("click", () => {
+    console.log("button clicked");
+  });
+});
 
-    const user = reactive({
-      name: "Harry",
-      age: 20,
-    });
+const user = reactive({
+  name: "Harry",
+  age: 20,
+});
 
-    setTimeout(() => {
-      user.name = "Potter";
-    }, 3000);
+setTimeout(() => {
+  user.name = "Potter";
+}, 3000);
 
-    const { num, increment, double } = useNumber();
-    const { phrase, reversedPhrase, num: phraseNum } = usePhrase();
+const { num, increment, double } = useNumber();
+const { phrase, reversedPhrase, num: phraseNum } = usePhrase();
 
-    return {
-      num,
-      increment,
-      ...toRefs(user),
-      phrase,
-      reversedPhrase,
-      double,
-      user,
-      btn,
-      phraseNum,
-    };
-  },
-};
+const { name } = toRefs(user);
 </script>
