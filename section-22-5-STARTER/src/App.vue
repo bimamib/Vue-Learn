76b42bcbd99d6107593258d1818e01237ff65dba
ʙ ɪ ᴍ ᴀ ᴀ ᴀ <bimaprast1601@gmail.com>
2024-11-07 17:48:44 +0700

    Feat: Encapsulating Scrolling

--- section-22-5-STARTER/src/App.vue
+++ section-22-5-STARTER/src/App.vue
@@ -1,11 +1,130 @@
 <template>
   <div id="app">
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin dapibus ac
+      velit id condimentum. Phasellus placerat massa lacinia sapien euismod, ut
+      vestibulum felis feugiat. Nam non aliquam purus, at rhoncus justo.
+      Vestibulum magna lacus, lobortis et auctor ut, maximus vitae augue.
+      Vestibulum accumsan eros non dignissim scelerisque. Donec eu tristique
+      erat. Proin ultricies gravida sapien at viverra.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin dapibus ac
+      velit id condimentum. Phasellus placerat massa lacinia sapien euismod, ut
+      vestibulum felis feugiat. Nam non aliquam purus, at rhoncus justo.
+      Vestibulum magna lacus, lobortis et auctor ut, maximus vitae augue.
+      Vestibulum accumsan eros non dignissim scelerisque. Donec eu tristique
+      erat. Proin ultricies gravida sapien at viverra.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin dapibus ac
+      velit id condimentum. Phasellus placerat massa lacinia sapien euismod, ut
+      vestibulum felis feugiat. Nam non aliquam purus, at rhoncus justo.
+      Vestibulum magna lacus, lobortis et auctor ut, maximus vitae augue.
+      Vestibulum accumsan eros non dignissim scelerisque. Donec eu tristique
+      erat. Proin ultricies gravida sapien at viverra.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin dapibus ac
+      velit id condimentum. Phasellus placerat massa lacinia sapien euismod, ut
+      vestibulum felis feugiat. Nam non aliquam purus, at rhoncus justo.
+      Vestibulum magna lacus, lobortis et auctor ut, maximus vitae augue.
+      Vestibulum accumsan eros non dignissim scelerisque. Donec eu tristique
+      erat. Proin ultricies gravida sapien at viverra.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin dapibus ac
+      velit id condimentum. Phasellus placerat massa lacinia sapien euismod, ut
+      vestibulum felis feugiat. Nam non aliquam purus, at rhoncus justo.
+      Vestibulum magna lacus, lobortis et auctor ut, maximus vitae augue.
+      Vestibulum accumsan eros non dignissim scelerisque. Donec eu tristique
+      erat. Proin ultricies gravida sapien at viverra.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin dapibus ac
+      velit id condimentum. Phasellus placerat massa lacinia sapien euismod, ut
+      vestibulum felis feugiat. Nam non aliquam purus, at rhoncus justo.
+      Vestibulum magna lacus, lobortis et auctor ut, maximus vitae augue.
+      Vestibulum accumsan eros non dignissim scelerisque. Donec eu tristique
+      erat. Proin ultricies gravida sapien at viverra.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin dapibus ac
+      velit id condimentum. Phasellus placerat massa lacinia sapien euismod, ut
+      vestibulum felis feugiat. Nam non aliquam purus, at rhoncus justo.
+      Vestibulum magna lacus, lobortis et auctor ut, maximus vitae augue.
+      Vestibulum accumsan eros non dignissim scelerisque. Donec eu tristique
+      erat. Proin ultricies gravida sapien at viverra.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin dapibus ac
+      velit id condimentum. Phasellus placerat massa lacinia sapien euismod, ut
+      vestibulum felis feugiat. Nam non aliquam purus, at rhoncus justo.
+      Vestibulum magna lacus, lobortis et auctor ut, maximus vitae augue.
+      Vestibulum accumsan eros non dignissim scelerisque. Donec eu tristique
+      erat. Proin ultricies gravida sapien at viverra.
+    </p>
     <div class="mt-2 ml-2">
       <button type="button" class="btn btn-primary" @click="open = true">
         Open
       </button>
     </div>
-
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin dapibus ac
+      velit id condimentum. Phasellus placerat massa lacinia sapien euismod, ut
+      vestibulum felis feugiat. Nam non aliquam purus, at rhoncus justo.
+      Vestibulum magna lacus, lobortis et auctor ut, maximus vitae augue.
+      Vestibulum accumsan eros non dignissim scelerisque. Donec eu tristique
+      erat. Proin ultricies gravida sapien at viverra.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin dapibus ac
+      velit id condimentum. Phasellus placerat massa lacinia sapien euismod, ut
+      vestibulum felis feugiat. Nam non aliquam purus, at rhoncus justo.
+      Vestibulum magna lacus, lobortis et auctor ut, maximus vitae augue.
+      Vestibulum accumsan eros non dignissim scelerisque. Donec eu tristique
+      erat. Proin ultricies gravida sapien at viverra.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin dapibus ac
+      velit id condimentum. Phasellus placerat massa lacinia sapien euismod, ut
+      vestibulum felis feugiat. Nam non aliquam purus, at rhoncus justo.
+      Vestibulum magna lacus, lobortis et auctor ut, maximus vitae augue.
+      Vestibulum accumsan eros non dignissim scelerisque. Donec eu tristique
+      erat. Proin ultricies gravida sapien at viverra.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin dapibus ac
+      velit id condimentum. Phasellus placerat massa lacinia sapien euismod, ut
+      vestibulum felis feugiat. Nam non aliquam purus, at rhoncus justo.
+      Vestibulum magna lacus, lobortis et auctor ut, maximus vitae augue.
+      Vestibulum accumsan eros non dignissim scelerisque. Donec eu tristique
+      erat. Proin ultricies gravida sapien at viverra.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin dapibus ac
+      velit id condimentum. Phasellus placerat massa lacinia sapien euismod, ut
+      vestibulum felis feugiat. Nam non aliquam purus, at rhoncus justo.
+      Vestibulum magna lacus, lobortis et auctor ut, maximus vitae augue.
+      Vestibulum accumsan eros non dignissim scelerisque. Donec eu tristique
+      erat. Proin ultricies gravida sapien at viverra.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin dapibus ac
+      velit id condimentum. Phasellus placerat massa lacinia sapien euismod, ut
+      vestibulum felis feugiat. Nam non aliquam purus, at rhoncus justo.
+      Vestibulum magna lacus, lobortis et auctor ut, maximus vitae augue.
+      Vestibulum accumsan eros non dignissim scelerisque. Donec eu tristique
+      erat. Proin ultricies gravida sapien at viverra.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin dapibus ac
+      velit id condimentum. Phasellus placerat massa lacinia sapien euismod, ut
+      vestibulum felis feugiat. Nam non aliquam purus, at rhoncus justo.
+      Vestibulum magna lacus, lobortis et auctor ut, maximus vitae augue.
+      Vestibulum accumsan eros non dignissim scelerisque. Donec eu tristique
+      erat. Proin ultricies gravida sapien at viverra.
+    </p>
     <app-modal :show="open" @hide="open = false" />
   </div>
 </template>
